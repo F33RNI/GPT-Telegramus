46e125062ba98efcd396cd62b63dc5f9cd494c94
F33RNI <xxoinvizionxx@gmail.com>
2023-05-21 05:58:15 +0300

    Fix API type 1

--- main.py
+++ main.py
@@ -32,7 +32,7 @@ import UsersHandler
 from JSONReaderWriter import load_json
 
 # GPT-Telegramus version
-__version__ = "2.1.2"
+__version__ = "2.1.3"
 
 # Logging level
 LOGGING_LEVEL = logging.INFO
