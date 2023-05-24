a5ccd3113d63155d795c1a4325c930e5d57099ff
F33RNI <xxoinvizionxx@gmail.com>
2023-05-24 21:39:12 +0300

    Update revChatGPT and EdgeGPT

--- main.py
+++ main.py
@@ -32,7 +32,7 @@ import UsersHandler
 from JSONReaderWriter import load_json
 
 # GPT-Telegramus version
-__version__ = "2.1.4"
+__version__ = "2.1.5"
 
 # Logging level
 LOGGING_LEVEL = logging.INFO
