659805cc5812c946f8bd0e2ed713e9dc4287ce58
F33RNI <xxoinvizionxx@gmail.com>
2023-11-30 18:09:59 +0300

    Bump version

--- main.py
+++ main.py
@@ -34,7 +34,7 @@ import UsersHandler
 from JSONReaderWriter import load_json
 
 # GPT-Telegramus version
-__version__ = "3.6.1"
+__version__ = "3.6.3"
 
 # Logging level
 LOGGING_LEVEL = logging.INFO
