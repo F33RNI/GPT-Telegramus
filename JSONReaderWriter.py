d596351ba9e136cad02c87f5db9b58a3430bd1b3
F33RNI <xxoinvizionxx@gmail.com>
2024-03-03 00:26:20 -0500

    refactor: change license headers

--- JSONReaderWriter.py
+++ JSONReaderWriter.py
@@ -1,18 +1,21 @@
 """
- Copyright (C) 2023 Fern Lane, GPT-Telegramus
- Licensed under the GNU Affero General Public License, Version 3.0 (the "License");
- you may not use this file except in compliance with the License.
- You may obtain a copy of the License at
-       https://www.gnu.org/licenses/agpl-3.0.en.html
- Unless required by applicable law or agreed to in writing, software
- distributed under the License is distributed on an "AS IS" BASIS,
- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- See the License for the specific language governing permissions and
- limitations under the License.
- IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY CLAIM, DAMAGES OR
- OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
- ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
- OTHER DEALINGS IN THE SOFTWARE.
+Copyright (C) 2023-2024 Fern Lane
+
+This file is part of the GPT-Telegramus distribution
+(see <https://github.com/F33RNI/GPT-Telegramus>)
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as
+published by the Free Software Foundation, either version 3 of the
+License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
 """
 
 import json
